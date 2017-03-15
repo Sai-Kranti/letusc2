5867bf5cbfd82492d96f5e775f1993f78cf1a5f3
Sai-Kranti <p.saikranti@gmail.com>
2017-03-15 10:33:47 +0530

    testing

---
+++ app/src/main/java/com/example/android/nav/activity/NotificationsActivity.java
@@ -0,0 +1,128 @@
+package com.example.android.nav.activity;
+
+/**
+ * Created by psaik on 3/13/2017.
+ */
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.android.nav.R;
+import com.example.android.nav.fragments.Feed;
+import com.example.android.nav.fragments.Articles;
+import com.example.android.nav.fragments.Blogs;
+import com.example.android.nav.fragments.GovernmentPolicies;
+import com.example.android.nav.fragments.Growth;
+import com.example.android.nav.fragments.Programs;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+
+        // toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //  setSupportActionBar(toolbar);
+
+        //  getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+   //     mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new GovernmentPolicies(), "Government Policies");
+        adapter.addFragment(new Growth(), "Growth");
+        adapter.addFragment(new Programs(), "Programs");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_news:
+                    startActivity(new Intent(NotificationsActivity.this, NewsActivity.class));
+                    return true;
+                case R.id.navigation_notifications:
+                    startActivity(new Intent (NotificationsActivity.this, NotificationsActivity.class));
+                    return true;
+                case R.id.navigation_incubator:
+                    mTextMessage.setText(R.string.title_incubator);
+                    return true;
+                case R.id.navigation_fortuneteller:
+                    mTextMessage.setText(R.string.title_fortuneteller);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/android/nav/fragments/Blogs.java
@@ -0,0 +1,37 @@
+package com.example.android.nav.fragments;
+
+/**
+ * Created by psaik on 3/13/2017.
+ */
+
+
+
+
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import com.example.android.nav.R;
+
+
+public class Blogs extends Fragment{
+
+    public Blogs() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blogs, container, false);
+    }
+
+}
